2d1e9928b2e9cd6d89e9d02a27bbe422839435b4
Justice Payne <spikepayne@yahoo.com>
2020-08-30 14:53:17 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,5 +16,5 @@ end
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
-  shipping_manifest = ["gun powder"] => 4
+  shipping_manifest = ["gun powder"] = 4
 end
