1b272c355de5beae5b3cd4ef5d6dc3dddf855992
Justice Payne <spikepayne@yahoo.com>
2020-08-30 14:25:04 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,11 +27,4 @@ def adding
   shipping_manifest["muskests"] = 2
 shipping_manifest["gun powder"] = 4
 
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
 end
